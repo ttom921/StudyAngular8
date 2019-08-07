96b618a438d201841456f16ace5fb26138fc5df7
ttom921 <ttom921@hotmail.com>
2019-08-07 15:51:04 +0800

    更新程式

--- ngtest01/src/app/demo.service.spec.ts
+++ ngtest01/src/app/demo.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { DemoService } from './demo.service';
 
 describe('DemoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+
+  }));
 
   it('should be created', () => {
     const service: DemoService = TestBed.get(DemoService);
