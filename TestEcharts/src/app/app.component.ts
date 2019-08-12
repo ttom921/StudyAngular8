c02544fd7b560ff53f265e2bfff023682e23f577
ttom921 <ttom921@hotmail.com>
2019-08-12 15:08:47 +0800

    加入顯示簡單圖表顯示

---
+++ TestEcharts/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { EChartOption } from 'echarts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  chartOption: EChartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [{
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: 'line'
+    }]
+  }
+
+  title = 'TestEcharts';
+}
