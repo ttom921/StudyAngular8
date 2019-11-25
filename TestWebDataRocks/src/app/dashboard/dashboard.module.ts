caafca7930e361c87565207fce06414bda0dbb51
ttom921 <ttom921@hotmail.com>
2019-11-25 16:01:18 +0800

    加入可以顯示WebDataRocks的資料

--- TestWebDataRocks/src/app/dashboard/dashboard.module.ts
+++ TestWebDataRocks/src/app/dashboard/dashboard.module.ts
@@ -6,10 +6,19 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedAngularMaterialModule } from '../share/shared-angular-material/shared-angular-material.module';
 import { DashboardComponent } from './dashboard.component';
 import { TestShowPageComponent } from './page/test-show-page/test-show-page.component';
+import { WebDataRocksPivot } from '../webdatarocks/webdatarocks.angular4';
+import { TestWebdatarockspivotComponent } from './page/test-webdatarockspivot/test-webdatarockspivot.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, HeaderComponent, SidebarComponent, TestShowPageComponent],
+  declarations: [
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    TestShowPageComponent,
+    WebDataRocksPivot,
+    TestWebdatarockspivotComponent,
+  ],
   imports: [
     SharedAngularMaterialModule,
     DashboardRoutingModule
