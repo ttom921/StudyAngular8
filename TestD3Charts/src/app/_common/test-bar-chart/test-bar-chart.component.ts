4e248016ce4215971849e322fe32624f05917666
ttom921 <ttom921@hotmail.com>
2020-02-20 15:18:54 +0800

    更新程式碼

--- TestD3Charts/src/app/_common/test-bar-chart/test-bar-chart.component.ts
+++ TestD3Charts/src/app/_common/test-bar-chart/test-bar-chart.component.ts
@@ -1,29 +1,80 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
+import { fromEvent } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-test-bar-chart',
   templateUrl: './test-bar-chart.component.html',
-  styleUrls: ['./test-bar-chart.component.scss']
+  styleUrls: ['./test-bar-chart.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TestBarChartComponent implements OnInit, AfterViewInit {
 
   @ViewChild('chart', { static: true }) private chartContainer: ElementRef;
   @ViewChild('svg', { static: true }) svgRef: ElementRef<SVGElement>;
   loading = false;
+  //svg相關
+  private svg: any;
+  //div的大小可變
+  private width: number;
+  private height: number;
+  private margin = { top: 8, right: 10, bottom: 32, left: 20 };
+  //svg viewbox的大小
+  private viewboxWidth = 800;
+  private viewboxHeight = 100;
   constructor() { }
 
   ngOnInit() {
   }
   ngAfterViewInit(): void {
-    const { width, height } = this.chartContainer.nativeElement.getBoundingClientRect();
-    const margin = 20;
-    const svg = d3.select(this.svgRef.nativeElement);
-    this.drawChart(svg, width, height, margin);
+    //選到svg標籤
+    this.svg = d3.select(this.svgRef.nativeElement);
+    this.drawChart();
   }
-  drawChart(svg: any, width: number, height: number, margin: number) {
-
-    const chartWidth = width - 2 * margin;
-    const chartHeight = height - 2 * margin;
+  //計算寬高
+  private calWidthHeightMargin() {
+    const { width, height } = this.chartContainer.nativeElement.getBoundingClientRect();
+    this.width = width;
+    this.height = height;
+  }
+  //初始化svg
+  private initSvg(visiable = false) {
+    //建立svg
+    let svg = this.svg;
+    svg
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .attr('viewBox', `0 0 ${this.viewboxWidth} ${this.viewboxHeight}`)
+      .attr('preserveAspectRatio', 'xMinYMid')
+      ;
+    svg.selectAll('g').remove();
+    //視窗有resize的事件
+    fromEvent(window, 'resize')
+      .pipe(
+        tap(() => this.loading = true),
+        debounceTime(300)
+      )
+      .subscribe(() => {
+        //this.calWidthHeightMargin();
+        this.drawChart();
+        this.loading = false;
+      });
+    // //查看大小
+    if (visiable == false) return;
+    svg.append('g').append('rect')
+      .style('fill', 'purple')
+      .style('fill-opacity', 0.1)
+      .style('stroke', 'purple')
+      .style('stroke-width', 4)
+      .attr('width', this.width)
+      .attr('height', this.height);
+  }
+  drawChart() {
+    this.calWidthHeightMargin();
+    this.initSvg(true);
+    let svg = this.svg;
+    // const chartWidth = width - 2 * margin;
+    // const chartHeight = height - 2 * margin;
 
     // Set of data
     var dataset = [5, 2, 9, 4, 5, 6, 7];
@@ -31,85 +82,52 @@ export class TestBarChartComponent implements OnInit, AfterViewInit {
     const colors = d3.scaleLinear().domain([0, dataset.length]).range(<any[]>['red', 'blue']);
     const n = dataset.length;
     const maxValue = d3.max(dataset);
-    // Create our SVG container
-    svg
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .attr('preserveAspectRatio', 'xMinYMin');
+
 
     //x方向的座標的設定
+    //X的對映
     var xScale = d3.scaleLinear()
-      .domain([0, dataset.length])
-      .range([0, chartWidth]);
+      .domain([0, dataset.length - 1])
+      .range([0, this.viewboxWidth]);
+    //x軸的顯示設定
+    //this.xAxis = d3.axisBottom(xScale).ticks(10).tickSize(10).tickPadding(10).tickFormat((d: any) => (d / this.gsensorgen) + 's');
+    const xAxis = d3.axisBottom(xScale).ticks(dataset.length - 1).tickSize(10).tickPadding(10).tickFormat((d: any, i: any) => i);
+
     svg.append('g')
       .attr('class', 'x axis')
-      .attr('transform', `translate(${margin}, ${chartHeight + margin})`)
-      .call(d3.axisBottom(xScale));
+      .attr('transform', `translate(${this.margin.left}, ${this.viewboxHeight - this.margin.bottom})`)
+      .call(xAxis);
+
     //y方向的座標的設定
+    const subheight = this.margin.top + this.margin.bottom;
     var yScale = d3.scaleLinear()
       .domain([0, maxValue])
-      .range([0, chartHeight]);
+      .range([0, this.viewboxHeight - subheight]);
 
     var yScale2 = d3.scaleLinear()
       .domain([0, maxValue])
-      .range([chartHeight, 0]);
-
+      .range([this.viewboxHeight - subheight, 0]);
+    const yAxis = d3.axisLeft(yScale2).ticks(Math.min(Math.floor(this.viewboxHeight / 50), maxValue));
     svg.append('g')
       .attr('class', 'y axis')
-      .attr('transform', `translate(${margin}, ${margin})`)
-      .call(d3.axisLeft(yScale2).ticks(Math.min(Math.floor(chartHeight / 50), maxValue)));
-    //建立bar
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+      .call(yAxis);
+
+    // //建立bar
     var bars = svg.append('g')
       .attr('class', 'bars');
 
-    // Bind data to chart, and create bars
+    //Bind data to chart, and create bars
     bars.selectAll('rect')
       .data(dataset)
       .enter()
       .append('rect')
       .style('fill', (d, i) => colors(i))
-      .attr('x', (d, i) => xScale(i) + margin)
-      .attr('y', (d) => chartHeight + margin - yScale(d))//使正常顯示
+      .attr('x', (d, i) => xScale(i) + this.margin.left / 2)
+      .attr('y', (d) => this.viewboxHeight - this.margin.bottom - yScale(d))//使正常顯示
       .attr('width', 20)
       .attr('height', (d, i) => {
-        return yScale(d);//放火顯示
+        return yScale(d);//以比例顯示
       });
-
-    // data.forEach((d, i) => {
-    //   console.log(d);
-    //   //console.log(xScale(i));
-    //   //console.log(yScale(i));
-    //   // svg
-    //   //   .append('rect')
-    //   //   .attr('transform', `translate(${margin + xScale(i) + xScale(1) / 2}, ${margin})`)
-    //   //   .style('fill', colors[i % 2])
-    //   //   .attr('width', barWidth)
-    //   //   .attr('height', 0)
-    //   //   .transition()
-    //   //   .attr('height', d * 10);
-    //   svg
-    //     .append('rect')
-    //     .attr('x', `${margin + xScale(i)}`)
-    //     .attr('y', (d) => `${chartWidth}-d`)
-    //     .attr('width', barWidth)
-    //     .style('fill', colors[i % 2])
-    //     .attr('height', d * 10);
-
-    // });
-
-    // //
-    // update
-    //   .enter()
-    //   .append('rect')
-    //   .attr('class', 'bar')
-    //   .attr('x', d => xScale(d))
-    //   .attr('y', d => yScale(0))
-    //   .attr('width', xScale)
-    //   .attr('height', 0)
-    //   .style('fill', (d, i) => colors[i])
-    //   .transition()
-    //   .delay((d, i) => i * 10)
-    //   .attr('y', d => yScale(d[1]))
-    //   .attr('height', d => 10);
-
   }
 }
