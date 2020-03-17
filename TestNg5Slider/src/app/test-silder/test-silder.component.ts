9aef4353cf6bd06031e30a2aa2ca0a2397275b7b
ttom921 <ttom921@hotmail.com>
2020-03-17 09:02:35 +0800

    更新程式碼

--- TestNg5Slider/src/app/test-silder/test-silder.component.ts
+++ TestNg5Slider/src/app/test-silder/test-silder.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation, ElementRef, AfterViewInit } from '@angular/core';
 import { MatSlider, MatSliderChange } from '@angular/material/slider';
+import { MatCard } from '@angular/material/card';
 
 //import * as d3 from 'd3';
 declare var d3: any;
@@ -10,9 +11,10 @@ declare var d3: any;
   styleUrls: ['./test-silder.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class TestSilderComponent implements OnInit {
+export class TestSilderComponent implements OnInit, AfterViewInit {
   @ViewChild('slider', { static: true }) slider: MatSlider;
   @ViewChild('totalslider', { static: true }) totalslider: MatSlider;
+  @ViewChild('chart_container', { static: true }) chart_container: ElementRef;
 
   //全部的容量
   totalDisk: number = 800;
@@ -22,23 +24,42 @@ export class TestSilderComponent implements OnInit {
   //各車容量最大限制
   maxLimit: number = 50;
 
+  //
+  d3slider: any;
+  sliderFill: any;
   constructor() { }
 
+
   ngOnInit() {
+    //console.log(this.chart_container);
+    //console.log(this.chart_container.nativeElement);
+    //console.log(this.chart_container.nativeElement.offsetWidth);
+
     //計算各車的最大容量限制
     this.calMaxLimit();
     this.slider.value = this.maxLimit;
     this.totalslider.value = this.calTotalRemindDisk();
     this.initD3();
+    this.initFillD3();
+  }
+  ngAfterViewInit(): void {
+    console.log(this.chart_container.nativeElement.offsetWidth);
+    console.log(this.chart_container.nativeElement.offsetHeight);
+    //console.log(this.chart_container);
+    //console.log(this.chart_container);
+    //console.dir(this.chart_container);
   }
   onInputChange(event: MatSliderChange) {
     //console.log("This is emitted as the thumb slides");
     //console.log(event.value);
     //console.log(this.slider);
     this.slider.value = event.value;
+
     if (event.value >= this.maxLimit) {
       this.slider.value = this.maxLimit;
     }
+    //this.d3slider.value(this.slider.value);
+    this.setD3SliderValue(this.slider.value);
     //this.calTotalRemindDisk();
 
     this.totalslider.value = this.calTotalRemindDisk();
@@ -56,25 +77,64 @@ export class TestSilderComponent implements OnInit {
     //console.log("目前車子容量=" + this.slider.value);
     return Math.round(this.regCar * this.slider.value);
   }
+  setD3SliderValue(value) {
+    this.d3slider.value(this.slider.value);
+    this.sliderFill.value(this.slider.value);
+  }
   initD3() {
+
     //console.log(d3);
-    var slider = d3
+    this.d3slider = d3
       .sliderHorizontal()
       .min(10)
       .max(100)
       .step(1)
-      .width(300)
+      .width(260)
       .displayValue(false)
       .on('onchange', val => {
         d3.select('#value').text(val);
-      });
+      })
+      .default(this.slider.value)
+      ;
+    d3.select('#value').text(this.slider.value);
+    //console.dir(this.d3slider);
 
     d3.select('#slider')
       .append('svg')
-      .attr('width', 500)
-      .attr('height', 100)
+      .attr('width', 360)
+      .attr('height', 160)
+      .attr("viewBox", `0 0 300 100`)
+      .attr("preserveAspectRatio", `xMidYMid`)
       .append('g')
-      .attr('transform', 'translate(30,30)')
-      .call(slider);
+      .attr('transform', 'translate(20,50)')
+      .call(this.d3slider);
+  }
+  initFillD3() {
+    //console.log(d3);
+    this.sliderFill = d3
+      .sliderHorizontal()
+      .min(10)
+      .max(100)
+      .step(1)
+      .width(260)
+      .displayValue(false)
+      .fill('#2196f3')
+      .on('onchange', val => {
+        //d3.select('#value').text(val);
+      })
+      .default(this.slider.value)
+      ;
+    //d3.select('#value').text(this.slider.value);
+    //console.dir(this.d3slider);
+
+    d3.select('#sliderFill')
+      .append('svg')
+      .attr('width', 360)
+      .attr('height', 160)
+      .attr("viewBox", `0 0 300 100`)
+      .attr("preserveAspectRatio", `xMidYMid`)
+      .append('g')
+      .attr('transform', 'translate(20,50)')
+      .call(this.sliderFill);
   }
 }
